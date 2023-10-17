bf1fab095e01b6ecd15037ef4f9be20154ebdb35
Jakub Pająk <jakubpajak@MacBook-Pro-Jakub.local>
2023-10-17 13:06:22 +0200

    initializing repo

---
+++ AstroScreen_Cinema/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AstroScreen_Cinema.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
