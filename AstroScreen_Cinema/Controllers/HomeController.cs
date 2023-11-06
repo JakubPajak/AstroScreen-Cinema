e9beb54ddcf4c33a1d56e7a50df12ae8e9658f22
Jakub Pająk <jakubpajak@MacBook-Pro-Jakub.local>
2023-11-06 21:47:35 +0100

    Front Corrected

--- AstroScreen_Cinema/Controllers/HomeController.cs
+++ AstroScreen_Cinema/Controllers/HomeController.cs
@@ -23,6 +23,12 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult NowShowing()
+    {
+        return View();
+    }
+
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
