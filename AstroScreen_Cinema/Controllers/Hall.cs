e22ed4f44f4c4c6a7667c36b7ca605b24169c7ce
Luucas237 <lukas.grabarski02@gmail.com>
2023-11-19 21:32:17 +0100

    'NowShowingAdded'

---
+++ AstroScreen_Cinema/Controllers/Hall.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using AstroScreen_Cinema.Models;
+using System;
+
+
+namespace AstroScreen_Cinema.Controllers
+{
+    public class Hall : Controller
+    {
+        public IActionResult Hall_1()
+        {
+            return View();
+        }
+    }
+}
